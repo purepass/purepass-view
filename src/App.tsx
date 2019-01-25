29b7ef8fee7b9f71df527c8b8fbd9f0fa3252b8e
Matt Johnston <m@ttjohnston.com>
2019-01-25 08:07:00 -0400

    debounce, copy to clipboard, reset on blank secret

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import copy from 'copy-to-clipboard';
+import Erric from 'easy-e';
+import debounce from 'lodash.debounce';
 import { Purepass, PurepassOptions } from 'purepass-core';
 import * as validate from 'purepass-validators';
 import * as React from 'react';
+import ReactSVG from 'react-svg';
 import { Form } from 'semantic-ui-react';
+
+
 
 const purepass = new Purepass();
 
@@ -10,6 +16,8 @@ export interface IProps {
 }
 
 interface IState {
+  copyIconColor:string;
+  copyIconWidth:string;
   maxPasswordLength: number;
   maxPasswordLengthError: null | any;
   namespace: string;
@@ -19,13 +27,17 @@ interface IState {
   specialCharacter: string;
   specialCharacterError: null | any;
 }
-
+const defaultCopyIconColor: string = '#A9A9A9'
+const activelyCopying: string = 'rgb(17,138,22)'
+const defaultCopyIconWidth: string = '24px'
 class App extends React.Component<IProps, IState> {
   public formStyle = {display:'flex', flexDirection:'column', justifyContent:'spaceBetween', alignItems:'center'}
-
+  public namespaceDebounceInterval:number = 400 // may be generous
   constructor(props: IProps) {
     super(props);
     this.state = {
+      copyIconColor: defaultCopyIconColor,
+      copyIconWidth: defaultCopyIconWidth,
       maxPasswordLength: 64,
       maxPasswordLengthError:null,
       namespace: 'Un',
@@ -38,38 +50,60 @@ class App extends React.Component<IProps, IState> {
   }
   
 
-  public handleSecretChange = (ev: React.FormEvent<HTMLInputElement>) => {
-    const val = ev.currentTarget.value;
+  public handleSecretChange = (val: string) => {
     const error = validate.secret(val);
     if(error) {
-      this.setState({ secretError: error.messageForHumans });
+      this.setState({ secret: '', secretError: error.messageForHumans });
       return;
     }
     this.setState({ secret: val, secretError: null });
   }
 
-  public handleNamespaceChange = (ev: React.FormEvent<HTMLInputElement>) => {
-    const val = ev.currentTarget.value;
-    const error = validate.namespace(val);
-    if(error) {
-      this.setState({ namespaceError: error.messageForHumans });
-      return;
-    }
-    this.setState({ namespace: val, namespaceError: null });
+  public handleNamespaceChange = (val: string) => {
+    const debounceUpdate = debounce((value) => {
+      const error = validate.namespace(value)
+      const namespaceNull = (!value || !value.length)
+      if(!namespaceNull && error) {
+        // next line assumes namespace is defined
+        if(error.code === 'purepass/validators/validate.namespace/(namespace.length!==2)') {
+          error.setMessageForHumans('namespace must be left blank or 2 characters in length')
+        }
+        this.setState({ namespaceError: error.messageForHumans });
+        return
+      }
+      this.setState({ namespace: val, namespaceError: null })
+  }, this.namespaceDebounceInterval)
+  debounceUpdate(val);
+}
+
+  public handleMaxPasswordLengthChange = (valStr: string) => {
+
+    const debounceUpdate = debounce((val) => {
+      if(val === ''){
+        this.setState({ maxPasswordLength: 64, maxPasswordLengthError: null });
+        return
+      }
+      const value = parseInt(valStr, 10)
+      const error = validate.maxPasswordLength(value);
+      if(error) {
+        if(error.code === 'purepass/valudateArgs/(maxPasswordLength<=13)') {
+          error.setMessageForHumans('max password length should be at least 13 characters')
+        }
+        this.setState({ maxPasswordLengthError: error.messageForHumans });
+        return
+      }
+
+      if(Number.isNaN(value)) {
+        const typeError = new Erric('purepass-view/handleMaxPasswordLengthChange/input===NaN','max password length must be an integer',{input: valStr})
+        this.setState({ maxPasswordLengthError: typeError.messageForHumans });
+        return;
+      }
+      this.setState({ maxPasswordLength: value, maxPasswordLengthError: null });
+  }, this.namespaceDebounceInterval)
+    debounceUpdate(valStr);
   }
 
-  public handleMaxPasswordLengthChange = (ev: React.FormEvent<HTMLInputElement>) => {
-    const val = parseInt(ev.currentTarget.value, 10);
-    const error = validate.maxPasswordLength(val);
-    if(error) {
-      this.setState({ maxPasswordLengthError: error.messageForHumans });
-      return;
-    }
-    this.setState({ maxPasswordLength: val, maxPasswordLengthError: null });
-  }
-
-  public handleSpecialCharacterChange = (ev: React.FormEvent<HTMLInputElement>) => {
-    const val = ev.currentTarget.value;
+  public handleSpecialCharacterChange = (val: string) => {
     const error = validate.specialCharacter(val);
     if(error) {
       this.setState({ specialCharacterError: error.messageForHumans });
@@ -78,21 +112,35 @@ class App extends React.Component<IProps, IState> {
     this.setState({ specialCharacter: val, specialCharacterError: null });
   }
   
+  public copyToClipboardWithFeedback = (generatedPassword: string) => {
+    copy(generatedPassword);
+
+    this.setState({copyIconColor: activelyCopying, copyIconWidth:'30px'},() => {
+      setInterval(()=>{this.setState({copyIconColor:defaultCopyIconColor, copyIconWidth: defaultCopyIconWidth})},300)
+    });
+  }
+
   public renderPurepass = () => {
-    const { secret, namespace, specialCharacter, maxPasswordLength } = this.state;
-    if(!secret){
+    const { secret, namespace, specialCharacter, maxPasswordLength, copyIconColor, copyIconWidth } = this.state;
+    if(!secret || !secret.length){
       return (
         <div>Secret is required</div>
       )
     }
     const generatedPassword = purepass.generatePassword(secret, new PurepassOptions(namespace, specialCharacter, maxPasswordLength));
+
+    // tslint:disable-next-line:jsx-no-lambda
     return (
-      <div>
+      <div onClick={this.copyToClipboardWithFeedback.bind(this, generatedPassword, {debug: true})}>
         you're purepass is:
         <pre style={{fontSize:9}}>{generatedPassword}</pre>
+        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <ReactSVG src="/fa-copy-reg.svg" svgStyle={{ width: copyIconWidth, color: copyIconColor }}/>
+        </div>
       </div>
     );
   }
+
   public render() {
     return (
       <div style={{display:'flex', justifyContent:'center'}}>
@@ -101,25 +149,36 @@ class App extends React.Component<IProps, IState> {
           <Form style={this.formStyle}>
             <Form.Field>
               <label>secret</label>
-              <input placeholder="password1234" onChange={this.handleSecretChange} />
+              <input placeholder="password1234" onChange={
+                // tslint:disable-next-line:jsx-no-lambda
+                ({ target: { value } }) => { this.handleSecretChange(value) }} 
+              />
               <div style={{color:'red'}}>{this.state.secretError}</div>
             </Form.Field>
             
             <Form.Field>
               <label>namespace</label>
-              <input placeholder="Un" onChange={this.handleNamespaceChange} />
+              <input placeholder="Un" onChange={
+                // tslint:disable-next-line:jsx-no-lambda
+                ({ target: { value } }) => { this.handleNamespaceChange(value) }}  />
               <div style={{color:'red'}}>{this.state.namespaceError}</div>
             </Form.Field>
 
             <Form.Field>
               <label>max character count</label>
-              <input placeholder="64" onChange={this.handleMaxPasswordLengthChange}/>
+              <input placeholder="64" onChange={
+                // tslint:disable-next-line:jsx-no-lambda
+                ({ target: { value } }) => { this.handleMaxPasswordLengthChange(value) }}
+              />
               <div style={{color:'red'}}>{this.state.maxPasswordLengthError}</div>
             </Form.Field>
 
             <Form.Field>
               <label>special character</label>
-              <input placeholder="#" onChange={this.handleSpecialCharacterChange}/>
+              <input placeholder="#" onChange={
+                // tslint:disable-next-line:jsx-no-lambda
+                ({ target: { value } }) => { this.handleSpecialCharacterChange(value) }}
+              />
               <div style={{color:'red'}}>{this.state.specialCharacterError}</div>
             </Form.Field>
             <div style={{display:'flex', justifyContent:'center'}}>{this.renderPurepass()}</div>
